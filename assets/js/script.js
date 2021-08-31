9f99ccc5ca1a846ad986c5ef2bf7f838a1e74e27
Matthew Pandolfo <matt@pandolfo.com>
2021-08-30 22:10:31 -0400

    uv index

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,26 +1,55 @@
+var apiKey = "de81cbd7917a62a289c2cd964e2ccb23";
+
 var searchCityBtn = document.querySelector("#searchCityBtn");
 var selectedCityHeaderEl = document.querySelector("#currentCityHeder");
+var tempEl = document.querySelector("#displayTemp");
+var windEl = document.querySelector("#displayWind");
+var humidityEl = document.querySelector("#displayHumidity");
+var uvEl = document.querySelector("#displayUV");
 
 function searchCity(cityName) {
-  var apiKey = "de81cbd7917a62a289c2cd964e2ccb23";
   var apiUrl =
     "http://api.openweathermap.org/data/2.5/weather?q=" +
-    "rocky hill" +
+    "Australia" +
+    "&units=imperial" +
     "&appid=" +
     apiKey;
   fetch(apiUrl).then(function (response) {
     if (response.ok) {
       response.json().then(function (data) {
-        displayWeather(data);
+        selectedCityHeaderEl.textContent = data.name;
+        tempEl.textContent = data.main.temp + "°F";
+        windEl.textContent = data.wind.speed + " MPH";
+        humidityEl.textContent = data.main.humidity + "%";
+        getAllData(data);
       });
-    }else{
-      alert('Error: Please enter a valid city');
+    } else {
+      alert("Error: Please enter a valid city");
     }
   });
 }
 
-function displayWeather(city) {
-  selectedCityHeaderEl.textContent = city.name;
+function getAllData(data) {
+  var apiUrl =
+    "https://api.openweathermap.org/data/2.5/onecall?lat=" +
+    data.coord.lat +
+    "&lon=" +
+    data.coord.lon +
+    "&units=imperial&appid=" +
+    apiKey;
+  fetch(apiUrl).then(function (response) {
+    if (response.ok) {
+      response.json().then(function (data) {
+        console.log(apiUrl);
+        displayData(data);
+      });
+    }
+  });
+}
+
+function displayData(data) {
+  uvEl.textContent = data.current.uvi;
+  uvEl.classList = 'bg-success px-2 rounded text-white uvText';
 }
 
 searchCityBtn.addEventListener("click", searchCity);

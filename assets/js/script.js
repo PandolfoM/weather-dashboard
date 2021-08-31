264ffe0f5dc1614ed9a59f8a0667fc838996579d
Matthew Pandolfo <matt@pandolfo.com>
2021-08-31 10:20:56 -0400

    5 day forecast

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,11 +1,14 @@
 var apiKey = "de81cbd7917a62a289c2cd964e2ccb23";
 
 var searchCityBtn = document.querySelector("#searchCityBtn");
+// top part of data
 var selectedCityHeaderEl = document.querySelector("#currentCityHeder");
 var tempEl = document.querySelector("#displayTemp");
 var windEl = document.querySelector("#displayWind");
 var humidityEl = document.querySelector("#displayHumidity");
 var uvEl = document.querySelector("#displayUV");
+// 5 day forecast
+var forecastEl = document.querySelector("#forecast");
 
 var citySearchEl = document.querySelector("#cityInput");
 
@@ -21,11 +24,12 @@ function getCity(cityName) {
       response.json().then(function (data) {
         console.log(apiUrl);
         var date = new Date(data.dt * 1000);
-        var month = date.getMonth()+1;
+        // some reason the month is 1 behind :shrug:
+        var month = date.getMonth() + 1;
         var day = date.getDate();
         var year = date.getFullYear();
-        console.log(date);
-        selectedCityHeaderEl.textContent = data.name + " (" + month + '/' + day + '/' + year + ')';
+        selectedCityHeaderEl.textContent =
+          data.name + " (" + month + "/" + day + "/" + year + ")";
 
         var apiUrlAll =
           "https://api.openweathermap.org/data/2.5/onecall?lat=" +
@@ -36,11 +40,10 @@ function getCity(cityName) {
           apiKey;
         fetch(apiUrlAll).then(function (response) {
           response.json().then(function (data) {
-            tempEl.textContent = data.current.temp + "°F";
-            windEl.textContent = data.current.wind_speed + " MPH";
-            humidityEl.textContent = data.current.humidity + "%";
-            uvEl.textContent = data.current.uvi;
-            uvEl.classList = "bg-success px-2 rounded text-white uvText";
+            // display data
+            displayData(data);
+            // display 5 day forecast
+            displayForecast(data);
           });
         });
         // getAllData(data);
@@ -51,22 +54,53 @@ function getCity(cityName) {
   });
 }
 
-function getAllData(data) {
-  var apiUrlAll =
-    "https://api.openweathermap.org/data/2.5/onecall?lat=" +
-    data.coord.lat +
-    "&lon=" +
-    data.coord.lon +
-    "&units=imperial&appid=" +
-    apiKey;
-  fetch(apiUrl).then(function (response) {
-    if (response.ok) {
-      response.json().then(function (data) {
-        uvEl.textContent = data.current.uvi;
-        uvEl.classList = "bg-success px-2 rounded text-white uvText";
-      });
-    }
-  });
+function displayData(status) {
+  tempEl.textContent = status.current.temp + "°F";
+  windEl.textContent = status.current.wind_speed + " MPH";
+  humidityEl.textContent = status.current.humidity + "%";
+  uvEl.textContent = status.current.uvi;
+  uvEl.classList = "bg-success px-2 rounded text-white uvText";
+}
+
+function displayForecast(data) {
+  for (let i = 0; i < 5; i++) {
+    // console.log(data.daily[i].weather[0].icon);
+    // date
+    var date = new Date(data.daily[i].dt * 1000);
+    // some reason the month is 1 behind :shrug:
+    var month = date.getMonth() + 1;
+    var day = date.getDate();
+    var year = date.getFullYear();
+    
+    var displayForecast = document.createElement("div");
+    var forecastHeader = document.createElement("h3");
+    var forecastIcon = document.createElement("img");
+    var forecastTemp = document.createElement('p');
+    var forecastWind = document.createElement('p');
+    var forecastHum = document.createElement('p');
+    // div
+    displayForecast.classList = "col bg-primary whiteText";
+    // header
+    forecastHeader.textContent = month + "/" + day + "/" + year;
+    // icon
+    var iconCode = data.daily[i].weather[0].icon;
+    var iconUrl = 'http://openweathermap.org/img/w/' + iconCode + '.png';
+    forecastIcon.src = iconUrl;
+    forecastIcon.alt = data.daily[i].weather[0].main;
+    // temp
+    forecastTemp.textContent = data.daily[i].temp.day + " °F";
+    // wind
+    forecastWind.textContent = data.daily[i].wind_speed + " MPH";
+    // humidity
+    forecastHum.textContent = data.daily[i].humidity + ' %';
+    // append elements
+    forecastEl.appendChild(displayForecast);
+    displayForecast.appendChild(forecastHeader);
+    displayForecast.appendChild(forecastIcon);
+    displayForecast.appendChild(forecastTemp);
+    displayForecast.appendChild(forecastWind);
+    displayForecast.appendChild(forecastHum);
+  }
 }
 
 function searchCity() {

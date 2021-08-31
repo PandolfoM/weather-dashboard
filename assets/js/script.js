7d72d6cec5c04fd18fb197e226add48308185cc7
Matthew Pandolfo <matt@pandolfo.com>
2021-08-31 08:52:14 -0400

    display all data

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,21 +7,43 @@ var windEl = document.querySelector("#displayWind");
 var humidityEl = document.querySelector("#displayHumidity");
 var uvEl = document.querySelector("#displayUV");
 
-function searchCity(cityName) {
+var citySearchEl = document.querySelector("#cityInput");
+
+function getCity(cityName) {
   var apiUrl =
     "http://api.openweathermap.org/data/2.5/weather?q=" +
-    "Australia" +
+    cityName +
     "&units=imperial" +
     "&appid=" +
     apiKey;
   fetch(apiUrl).then(function (response) {
     if (response.ok) {
       response.json().then(function (data) {
-        selectedCityHeaderEl.textContent = data.name;
-        tempEl.textContent = data.main.temp + "°F";
-        windEl.textContent = data.wind.speed + " MPH";
-        humidityEl.textContent = data.main.humidity + "%";
-        getAllData(data);
+        console.log(apiUrl);
+        var date = new Date(data.dt * 1000);
+        var month = date.getMonth()+1;
+        var day = date.getDate();
+        var year = date.getFullYear();
+        console.log(date);
+        selectedCityHeaderEl.textContent = data.name + " (" + month + '/' + day + '/' + year + ')';
+
+        var apiUrlAll =
+          "https://api.openweathermap.org/data/2.5/onecall?lat=" +
+          data.coord.lat +
+          "&lon=" +
+          data.coord.lon +
+          "&units=imperial&appid=" +
+          apiKey;
+        fetch(apiUrlAll).then(function (response) {
+          response.json().then(function (data) {
+            tempEl.textContent = data.current.temp + "°F";
+            windEl.textContent = data.current.wind_speed + " MPH";
+            humidityEl.textContent = data.current.humidity + "%";
+            uvEl.textContent = data.current.uvi;
+            uvEl.classList = "bg-success px-2 rounded text-white uvText";
+          });
+        });
+        // getAllData(data);
       });
     } else {
       alert("Error: Please enter a valid city");
@@ -30,7 +52,7 @@ function searchCity(cityName) {
 }
 
 function getAllData(data) {
-  var apiUrl =
+  var apiUrlAll =
     "https://api.openweathermap.org/data/2.5/onecall?lat=" +
     data.coord.lat +
     "&lon=" +
@@ -40,16 +62,21 @@ function getAllData(data) {
   fetch(apiUrl).then(function (response) {
     if (response.ok) {
       response.json().then(function (data) {
-        console.log(apiUrl);
-        displayData(data);
+        uvEl.textContent = data.current.uvi;
+        uvEl.classList = "bg-success px-2 rounded text-white uvText";
       });
     }
   });
 }
 
-function displayData(data) {
-  uvEl.textContent = data.current.uvi;
-  uvEl.classList = 'bg-success px-2 rounded text-white uvText';
+function searchCity() {
+  var city = citySearchEl.value.trim();
+  var cityName = city.split(" ").join("%20");
+  if (cityName) {
+    getCity(cityName);
+  } else {
+    alert("Please enter a city");
+  }
 }
 
 searchCityBtn.addEventListener("click", searchCity);
